ecb9bbae8f2c8c31eae078164cab629f5f274b74
SeungHwan <a77285952@gmail.com>
2023-12-14 12:58:42 +0900

    calculator

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Calculator
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtResult = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnEqual = new System.Windows.Forms.Button();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.tbInput2 = new System.Windows.Forms.TextBox();
+            this.tbInput1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtResult
+            // 
+            this.txtResult.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.txtResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtResult.Location = new System.Drawing.Point(7, 21);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtResult.Size = new System.Drawing.Size(436, 92);
+            this.txtResult.TabIndex = 0;
+            this.txtResult.Text = "0";
+            this.txtResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.txtResult.Click += new System.EventHandler(this.txtResult_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnClear);
+            this.groupBox1.Controls.Add(this.btnEqual);
+            this.groupBox1.Controls.Add(this.btnPlus);
+            this.groupBox1.Controls.Add(this.btnDivide);
+            this.groupBox1.Controls.Add(this.btnMinus);
+            this.groupBox1.Controls.Add(this.btnMultiply);
+            this.groupBox1.Controls.Add(this.tbInput2);
+            this.groupBox1.Controls.Add(this.tbInput1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 132);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(431, 356);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(39, 257);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(261, 70);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnEqual
+            // 
+            this.btnEqual.Location = new System.Drawing.Point(322, 257);
+            this.btnEqual.Name = "btnEqual";
+            this.btnEqual.Size = new System.Drawing.Size(70, 70);
+            this.btnEqual.TabIndex = 8;
+            this.btnEqual.Text = "=";
+            this.btnEqual.UseVisualStyleBackColor = true;
+            this.btnEqual.Click += new System.EventHandler(this.btnEqual_Click);
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Location = new System.Drawing.Point(322, 165);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(70, 70);
+            this.btnPlus.TabIndex = 7;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Location = new System.Drawing.Point(230, 165);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(70, 70);
+            this.btnDivide.TabIndex = 6;
+            this.btnDivide.Text = "/";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Location = new System.Drawing.Point(134, 165);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(70, 70);
+            this.btnMinus.TabIndex = 5;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Location = new System.Drawing.Point(39, 165);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(70, 70);
+            this.btnMultiply.TabIndex = 4;
+            this.btnMultiply.Text = "X";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // tbInput2
+            // 
+            this.tbInput2.Location = new System.Drawing.Point(152, 81);
+            this.tbInput2.Name = "tbInput2";
+            this.tbInput2.Size = new System.Drawing.Size(240, 41);
+            this.tbInput2.TabIndex = 3;
+            // 
+            // tbInput1
+            // 
+            this.tbInput1.Location = new System.Drawing.Point(152, 34);
+            this.tbInput1.Name = "tbInput1";
+            this.tbInput1.Size = new System.Drawing.Size(240, 41);
+            this.tbInput1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "입력 2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "입력 1";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 514);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtResult);
+            this.Font = new System.Drawing.Font("굴림", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Margin = new System.Windows.Forms.Padding(7);
+            this.Name = "FrmMain";
+            this.Text = "Calculator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtResult;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnEqual;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.TextBox tbInput2;
+        private System.Windows.Forms.TextBox tbInput1;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        public enum EType
+        {
+            eNone = 0,
+            eEqual = 1,
+            ePlus,
+            eMinus,
+            eMultiply,
+            eDivide,
+
+            eNax = eDivide,
+        }
+
+        public EType m_Type = EType.eNone;
+        public float m_Result = 0.0f;
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            m_Type = EType.eMultiply;
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            m_Type = EType.eMinus;
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            m_Type = EType.eDivide;
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            m_Type = EType.ePlus;
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            if (m_Type == EType.eNone)
+                return;
+
+            float fA = float.Parse(tbInput1.Text);
+            float fB = float.Parse(tbInput2.Text);
+
+            m_Result = CalculateValue(m_Type, fA, fB);
+            txtResult.Text = string.Format("{0:F}", m_Result);
+        }
+
+        private float CalculateValue(EType type, float fA, float fB)
+        {
+            float fRes = 0;
+
+            switch (type)
+            {
+                case EType.ePlus:
+                    fRes = fA + fB;
+                    break;
+                case EType.eMinus:
+                    fRes = fA - fB;
+                    break;
+                case EType.eMultiply:
+                    fRes = fA * fB;
+                    break;
+                case EType.eDivide:
+                    fRes = fA / fB;
+                    break;
+            }
+
+            m_Type = EType.eNone;
+
+            return fRes;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void Clear()
+        {
+            m_Result = 0.0f;
+            txtResult.Text = "0";
+            tbInput1.Text = "";
+            tbInput2.Text = "";
+            m_Type = EType.eNone;
+        }
+
+        private void txtResult_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
